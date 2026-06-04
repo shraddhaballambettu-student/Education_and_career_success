c4dda91c33668cc384a9b722046e3c42e3f6be6d
shraddhaballambettu-student <shraddhaballambettu@gmail.com>
2026-06-04 13:10:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Career Success Platform",
+    page_icon="🎓",
+    layout="wide"
+)
+
+st.title("🎓 Education Career Success Analytics")
+
+st.markdown("""
+Analyze career growth, salary trends,
+promotion readiness and satisfaction metrics.
+""")
