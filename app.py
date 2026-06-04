0a1b6edf3c7da685fac57cc27a433b2320dc26f0
shraddhaballambettu-student <shraddhaballambettu@gmail.com>
2026-06-04 13:20:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,3 +12,24 @@ st.markdown("""
 Analyze career growth, salary trends,
 promotion readiness and satisfaction metrics.
 """)
+
+st.markdown("""
+<style>
+
+.block-container{
+    padding-top:1rem;
+}
+
+[data-testid="stMetric"]{
+    background:#f4f6f8;
+    padding:15px;
+    border-radius:12px;
+}
+
+div[data-testid="stPlotlyChart"]{
+    border-radius:12px;
+    padding:10px;
+}
+
+</style>
+""", unsafe_allow_html=True)
